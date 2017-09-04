65e070f426a4eb9802042c8f4d95b83124794106
Kumang <kumangxxx@gits.co.id>
2017-09-04 12:33:08 +0700

    backward compatible for older node version

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const amqp = require('amqplib/callback_api');
 const async = require('async');
 const uuid = require('uuid/v4');
@@ -7,38 +5,38 @@ const url = require('url');
 
 var global_channel;
 
-exports.setup = (url_string, callback) => {
+exports.setup = function(url_string, callback) {
     
     const uri = url.parse(url_string);
 
-    var connect = (cb) => {
-        amqp.connect(url_string, { servername: uri.hostname }, (err, conn) => {
+    var connect = function(cb) {
+        amqp.connect(url_string, { servername: uri.hostname }, function(err, conn) {
             cb(err, conn);
         });
     };
 
-    var open_channel = (conn, cb) => {
-        conn.createChannel((err, ch) => {
+    var open_channel = function(conn, cb) {
+        conn.createChannel(function(err, ch) {
             cb(err, ch);
         });
     };
 
-    async.waterfall([connect, open_channel], (err, channel) => {
+    async.waterfall([connect, open_channel], function(err, channel) {
         if (err) console.log(err);
         if (channel) global_channel = channel;
         callback(err);
     });
 };
 
-exports.callExchange = (exchange_name, route, string_body, timeout, callback) => {
+exports.callExchange = function(exchange_name, route, string_body, timeout, callback) {
     if (!global_channel) { console.log('RabbitRPC channel is not ready'); return; }
-    global_channel.assertQueue('', { exclusive: true }, (err, q) => {
+    global_channel.assertQueue('', { exclusive: true }, function(err, q) {
 
         let corr_id = uuid();
         var done = false;
         var timeoutHandle;
 
-        global_channel.consume(q.queue, (msg) => {
+        global_channel.consume(q.queue, function(msg) {
             if (msg.properties.correlationId == corr_id && !done) {
                 callback(null, msg.content.toString());
                 done = true;
@@ -47,7 +45,7 @@ exports.callExchange = (exchange_name, route, string_body, timeout, callback) =>
         }, { noAck: true });
 
         global_channel.publish(exchange_name, route, new Buffer(string_body), { correlationId: corr_id, replyTo: q.queue, contentType: "text/plain" });
-        timeoutHandle = setTimeout(() => {
+        timeoutHandle = setTimeout(function() {
             if (!done) {
                 callback({ code: 502, message: 'Request time out'});
                 done = true;
